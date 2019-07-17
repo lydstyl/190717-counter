c9ed9d7e6760673f2b115db81bd8bd2837e1773f
Gabriel Brun <lydstyl@gmail.com>
2019-07-18 00:52:56 +0200

    add item ok

--- src/App.js
+++ src/App.js
@@ -2,36 +2,47 @@ import React, { Component } from "react";
 import "./App.css";
 
 function Counter(props) {
-  return <div className="counter">{props.name}</div>;
+  return (
+    <div className="counter">
+      <div className="remove">Remove</div>
+      <div className="counterBox">
+        <div>{props.name}</div>
+        <div>{props.number}</div>
+      </div>
+    </div>
+  );
 }
 
 export default class App extends Component {
   addNew(e) {
     const inputVal = document.querySelector("input").value;
     if (inputVal) {
-      this.setState({ counters: [...this.state.counters, inputVal] });
+      this.setState({
+        counters: [...this.state.counters, { name: inputVal, number: 0 }]
+      });
     }
   }
 
   constructor(props) {
     super(props);
     this.state = {
-      counters: ["deja là", "one", "tow"]
+      counters: [{ name: "coco", number: 3 }]
     };
   }
 
   render() {
     const counters = [];
     for (let i = 0; i < this.state.counters.length; i++) {
-      const counter = this.state.counters[i];
-      counters.push(<Counter key={i} name={counter} />);
+      const counter = this.state.counters[i].name;
+      const number = this.state.counters[i].number;
+      counters.push(<Counter key={i} name={counter} number={number} />);
     }
     const Counters = <div className="counters">{counters}</div>;
 
     return (
       <div className="App">
         <h1>Counter</h1>
-        <input type="text" maxlength="10" />
+        <input type="text" maxLength="10" />
         <div className="new">New</div>
         <div
           onClick={e => {
