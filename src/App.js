da546efdfd8302fb5133de3f5bab623708aa7c8b
Gabriel Brun <lydstyl@gmail.com>
2019-07-17 23:14:35 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Counter(props) {
+  return <div className="counter">{props.name}</div>;
 }
 
-export default App;
+export default class App extends Component {
+  addNew(e) {
+    const inputVal = document.querySelector("input").value;
+    if (inputVal) {
+      this.setState({ counters: [...this.state.counters, inputVal] });
+    }
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      counters: ["deja là", "one", "tow"]
+    };
+  }
+
+  render() {
+    const counters = [];
+    for (let i = 0; i < this.state.counters.length; i++) {
+      const counter = this.state.counters[i];
+      counters.push(<Counter key={i} name={counter} />);
+    }
+    const Counters = <div className="counters">{counters}</div>;
+
+    return (
+      <div className="App">
+        <h1>Counter</h1>
+        <input type="text" maxlength="10" />
+        <div className="new">New</div>
+        <div
+          onClick={e => {
+            this.addNew(e);
+          }}
+        >
+          <div>
+            <br />
+          </div>
+        </div>
+        {Counters}
+      </div>
+    );
+  }
+}
