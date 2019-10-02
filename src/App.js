c33b53f14df4470b2bd28224a1a2dc75a75e7b51
Gabriel Brun <lydstyl@gmail.com>
2019-10-02 11:41:40 +0200

    css

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import "./App.css";
 function Counter(props) {
   return (
     <div className="counter" data-key={props.dataKey}>
-      <div onClick={props.handler} className="remove">
+      <div onClick={props.handler} className="button remove">
         Remove
       </div>
       <div className="counterBox" >
-        <div>{props.name}</div>
-        <div>{props.number}</div>
-        <div className='plus' onClick={props.increase}>+</div>
-        <div className='minus' onClick={props.decrease}>-</div>
+        <h2 className="name">{props.name}</h2>
+        <div className="number">{props.number}</div>
+        <div className='button plus' onClick={props.increase}>+</div>
+        <div className='button minus' onClick={props.decrease}>-</div>
       </div>
     </div>
   );
@@ -101,7 +101,7 @@ export default class App extends Component {
       <div className="App">
         <h1>Counter</h1>
         <input type="text" maxLength="10" value={this.state.inputText} onChange={e => this.handleChange(e)} />
-        <div className="new" onClick={e => 
+        <div className="button new" onClick={e => 
             this.addNew(e)
           }>New</div>
         
